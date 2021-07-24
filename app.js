eb41040991bc169b5790e3baff52449a8bce8f61
19alema <75095003+19alema@users.noreply.github.com>
2021-07-24 17:21:20 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const menu = document.querySelector(".menu");
+const head = document.querySelector("header")
+menu.onclick = function() {
+  if( head.style.height === "0vh"){
+     head.style.height = "100vh";
+  }
+  
+else {
+   head.style.height = "0vh";
+  }
+}
