d9da87a9a8894731ffb67f0469ba7363243d65bf
19alema <75095003+19alema@users.noreply.github.com>
2021-07-24 18:17:07 +0300

    jaupdate

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const menu = document.querySelector(".menu");
 const head = document.querySelector("header");
 
 
-console.log(navbar)
 menu.onclick = function() {
   if( head.style.height === "0vh"){
      head.style.height = "100vh";
